3cb2239335d852b346af0b41ebd7e9c9f7e712dd
lemongr <wackyomar@yahoo.com>
2021-12-17 15:21:05 +0200

    ''

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 console.log('Hello World');
-app.get('./root', function(req, res) {
+app.get('/', function(req, res) {
 	res.send('Hello Express');
 });
 
