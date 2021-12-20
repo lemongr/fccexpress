731a469db6c08f20dccb7b36014606dae742db00
lemongr <wackyomar@yahoo.com>
2021-12-20 13:55:58 +0200

    ''

--- myApp.js
+++ myApp.js
@@ -3,6 +3,7 @@ var app = express();
 app.get("/", function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
+app.use(/public, express.static(__dirname + /public));
 console.log('Hello World');
 
 
