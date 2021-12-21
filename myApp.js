8fd51b7c4f58b0ef7408112a7ee50cd43d13b659
lemongr <wackyomar@yahoo.com>
2021-12-21 13:21:34 +0200

    ''

--- myApp.js
+++ myApp.js
@@ -8,10 +8,7 @@ app.get("/", function(req, res) {
 app.get("/json", function(req, res) {
     res.json({"message": (process.env.MESSAGE_STYLE == 'uppercase' ? "HELLO JSON" : "Hello json")});
 });
-app.use(function(req, res) {
-  console.log(req.method + ' ' + req.path + ' ' + req.ip);
-  next();
-});
+
 app.use('/public', express.static(__dirname + '/public'));
 
 console.log('Hello World');
