ed6d036380910260fbb649c1e172c92094242615
lemongr <wackyomar@yahoo.com>
2021-12-21 13:27:29 +0200

    ''

--- myApp.js
+++ myApp.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
-
+app.use(function(req, res, next) {
+  console.log(req.method + ' ' + req.path + ' - ' + req.ip);
+  next();
+});
 app.get("/", function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
@@ -11,10 +14,7 @@ app.get("/json", function(req, res) {
 
 app.use('/public', express.static(__dirname + '/public'));
 
-app.use(function(req, res, next) {
-  console.log(req.method + ' ' + req.path + ' - ' + req.ip);
-  next();
-});
+
 
 console.log('Hello World');
 
