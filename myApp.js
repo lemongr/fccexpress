d9507df6faaa50ffd442872630049cc46d3c9b30
lemongr <wackyomar@yahoo.com>
2021-12-21 13:19:43 +0200

    ''

--- myApp.js
+++ myApp.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var app = express();
-app.use(function(req, res) {
-  console.log(req.method + ' ' + req.path + ' ' + req.ip);
-  next();
-});
+
 app.get("/", function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 app.get("/json", function(req, res) {
     res.json({"message": (process.env.MESSAGE_STYLE == 'uppercase' ? "HELLO JSON" : "Hello json")});
+});
+app.use(function(req, res) {
+  console.log(req.method + ' ' + req.path + ' ' + req.ip);
+  next();
 });
 app.use('/public', express.static(__dirname + '/public'));
 
